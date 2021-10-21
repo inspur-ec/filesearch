e6c9b85f71f77ade0aaeed2c668cb057ddf21398
zhengbin <rjguanwen001@163.com>
2021-10-21 17:10:09 +0800

    修复编译错误

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/inspur-ec/filesearch/conf"
-	"github.com/inspur-ec/filesearch/gui"
+	"filesearch/conf"
+	"filesearch/gui"
 	"github.com/olivere/elastic"
 	"log"
 	"os"
